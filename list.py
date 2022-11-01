5df01b6f337ccf2056fd18ffcd7269ef56baa3a4
@gustavo.lima <gustavo.lima@vitta.me>
2022-11-01 11:55:11 -0300

    feat: add Contact class to use objects and list array with for method

--- list.py
+++ list.py
@@ -1,1 +1,19 @@
-print("Init list of contacts")
+class Contact:
+    def execute(self):
+        self.name
+        self.age
+
+gustavo = Contact()
+gustavo.name = "Gustavo"
+gustavo.age = 18
+
+joao = Contact()
+joao.name = "Joao"
+joao.age = 17
+
+contacts = [gustavo, joao]
+
+for contact in contacts:
+    print("-------------------------------")
+    print("Contact with name:", contact.name)
+    print("Contact with age:", contact.age)
