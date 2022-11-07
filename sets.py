020397b3882f5a9118444d44e56f276b2f0c5834
@gustavo.lima <gustavo.lima@vitta.me>
2022-11-07 11:29:51 -0300

    feat: add sets examples

---
+++ sets.py
@@ -0,0 +1,10 @@
+ageFromContacts = {20}
+
+ageFromContacts.add(17)
+ageFromContacts.add(18)
+
+print("First ages:", ageFromContacts)
+
+ageFromContacts.remove(17)
+
+print("Second ages:", ageFromContacts)
